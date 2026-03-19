c490dded3864cf7e20d0a65196f13720b0d134a1
Sunny <sunny@azu.red>
2026-03-19 18:46:00 +0200

    feat: Add initial ignore patterns and database schema for highscores.

---
+++ database/dump.sql
@@ -0,0 +1,11 @@
+-- Mini-Lineage Database Dump
+CREATE TABLE IF NOT EXISTS `highscores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `total_xp` int(11) NOT NULL,
+  `race_id` int(11) NOT NULL,
+  `adena` int(11) NOT NULL,
+  `level` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
